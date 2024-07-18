e01642a3d5f2669f45c7c86fe9f8bd2fd032ed21
Dariush Najjarzade <dariush@najjarza.de>
2024-07-18 13:40:57 +0330

    fix(git-stage.sh): added fore to git add command

--- .local/bin-repo/git-stage.sh
+++ .local/bin-repo/git-stage.sh
@@ -6,7 +6,7 @@ RESET="Reset"
 ACTION=$(gum choose "$ADD" "$RESET")
 
 if [ "$ACTION" == "$ADD" ]; then
-    git status --short | cut -c 4- | pkgx gum choose --no-limit | xargs git add 
+    git status --short | cut -c 4- | pkgx gum choose --no-limit | xargs git add -f
 else
     git status --short | cut -c 4- | pkgx gum choose --no-limit | xargs git restore 
 fi
