df4b646633c72c73628f121d6fb8428a7c588626
Dariush Najjarzade <dnajjarzade+github@gmail.com>
2024-08-07 22:13:23 +0330

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -252,7 +252,8 @@ ansible_pull() {
     USER=$(whoami)
     # Print the current user
     echo "Current user is: $USER"
-    ansible-pull -C ansible -U https://github.com/DNajjarzade/vcsh_mr_template.git 'Documents/projects/personal/ansible/local.yml'
+    run_with_sudo ansible-pull -C ansible -U https://github.com/DNajjarzade/vcsh_mr_template.git 'Documents/projects/personal/ansible/superuser-play.yml'
+    ansible-pull -C ansible -U https://github.com/DNajjarzade/vcsh_mr_template.git 'Documents/projects/personal/ansible/user-play.yml'
 }
 trap ansible_pull EXIT
 
